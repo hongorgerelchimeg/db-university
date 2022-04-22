034ecce460e19dd3caa2cdd5644318dff47b6f27
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-04-22 18:30:29 +0200

    esercizio 2 ,3

--- index.php
+++ index.php
@@ -89,10 +89,23 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
+SELECT `degrees`.* , departments.name
+FROM `degrees` JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.id = 7;
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.*, CONCAT(`teachers`.`name`, ' ', `teachers`.`surname`) as Teacher
+FROM `teachers` 
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id` 
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.id = 44;
+
+
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
